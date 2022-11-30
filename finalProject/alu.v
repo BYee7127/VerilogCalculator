6121b131b69fa7cf67202338ca087173dc1ef167
BEVERLY YEE <byee7127@hotmail.com>
2022-11-29 21:20:12 -0700

    Added basic division.

--- finalProject/alu.v
+++ finalProject/alu.v
@@ -1,13 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Module Name:		alu 
+// Module Name:	alu 
 // Description: 	Module to select operation and caculate the result given 2 inputs.
 // 					All arithmetic calculations will be done in this module.
 //
 // Dependencies: 	a - the first operand
-//					b - the second operand
-//					out - the result from the calculation
-//					mode - what operation to carry out
-//					neg - whether the number is negative or not
+//						b - the second operand
+//						out - the result from the calculation
+//						mode - what operation to carry out
+//						neg - whether the number is negative or not
 //
 // Comments: 		This ALU will operate using the format "a OPERATION b = RESULT"
 //
@@ -15,8 +15,6 @@
 module alu(a, b, out, mode, neg);
 //module alu(a, b, mode, out);
 	// definitions of parameters, inputs, outputs, wires, regs...
-	//Let me take neg first to test
-	
 	input [15:0] a, b;
 	
 	// FPGA has only 10 switches: 8 will be numbers, 2 will be operations
@@ -26,10 +24,8 @@ module alu(a, b, out, mode, neg);
 	output reg [15:0] out;
 	output reg neg;
 
-	// variable names for modes to make things easier
-	
-	//set up initial state to 3'b100, dont need to declare 
-	
+	// hardcoded variables names for differing modes
+	// initial state defaulted to 3'b100
 	parameter ADD = 3'b000;
 	parameter SUB = 3'b001;
 	parameter MUL = 3'b010;
@@ -45,8 +41,8 @@ module alu(a, b, out, mode, neg);
 	wire tempA;
 	wire tempB;
 */
-	// do something whenever a or b changes -> may need more on the list
-	always@(a or b or mode)
+	// do something when an input changes
+	always@(a, b, mode)
 	begin
 		out = 16'b0000000000000000;
 
@@ -62,15 +58,18 @@ module alu(a, b, out, mode, neg);
 		else
 			negB = 0;
 */
+
+		// check 
+
 		case(mode)
-			ADD:	// 00
+			ADD:	// 000
 			begin
 				
 				out = a + b;
 				neg = 0;
 			end
 
-			SUB:	// 01
+			SUB:	// 001
 			begin
 				if(b > a)
 
@@ -89,16 +88,25 @@ module alu(a, b, out, mode, neg);
 
 			end
 
-			MUL:	// 10
+			MUL:	// 010
 			begin
 				out = a * b;
 				// neg = 0 only if both a and b are positive or negative
 				//neg = 0;
 			end
 
-			DIV:	// 11
+			DIV:	// 011
 			begin
-				// do nothing for now
+				// make sure to catch a divide by zero
+				if(b == 0 || a == 0)
+				begin
+					// have it return 0 for now
+					out = 0;
+				end
+				else
+				begin
+					out = a / b;
+				end
 			end
 		endcase
 	end
