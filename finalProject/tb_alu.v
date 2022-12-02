42d131ecd6bbfef9212f9b486bf29060f718cead
BEVERLY YEE <byee7127@hotmail.com>
2022-12-01 20:39:16 -0700

    Added 2 tests. Made clearer display statements.

--- finalProject/tb_alu.v
+++ finalProject/tb_alu.v
@@ -3,42 +3,64 @@
 
 module tb_alu;
 
-//Test pass
+	//Test pass
 
-parameter WIDTH = 8;
-reg [WIDTH -1:0] A,B;
-reg [1:0]  mode;
-wire [WIDTH -1: 0] out;
+/*
+	parameter WIDTH = 16;
+	reg [2:0]  mode;
+	wire [WIDTH -1: 0] out;
+	reg neg;
 
-alu AA(.a(A), .b(B), .mode(mode), .out(out));
+	alu AA(.a(A), .b(B), .mode(mo
+	reg [WIDTH -1:0] A,B;de), .out(out));
+*/
 
-initial begin
+	// Inputs
+	reg [15:0] A;
+	reg [15:0] B;
+	reg [2:0] mode;
+	
+	// Outputs
+	wire [15:0] out;
+	wire neg;
+	
+	// instantiate ALU
+	alu aa(.a(A), .b(B), .mode(mode), .out(out), .neg(neg));
+	
+	parameter ADD = 3'b000;
+	parameter SUB = 3'b001;
+	parameter MUL = 3'b010;
+	parameter DIV = 3'b011;
+	
+	initial begin
+		$display("Testing Add");
+		mode = ADD;
+		A = 8;
+		B = 9;
 
+		#15;
+		$display(A, "   +", B, "   =", out);
 
+		
+		A = 80;
+		B = 90;
+		#15;
+		$display(A, "   +", B, "   =", out);
 
-//test add
-mode = 2'b00;
-A = 8;
-B = 9;
-
-
-#15;
-$display("result is :%d", out);
-
-
-
-
-A = 80;
-B = 90;
-
-
-#15;
-$display("result is :%d", out);
-
-
-
-
-end
+		
+		$display("Testing Subtraction");
+		mode = SUB;
+		A = 9;
+		B = 7;
+		
+		#15;
+		$display(A, "   -", B, "   =", out);		
+		
+		A = 9;
+		B = 70;
+		
+		#15;
+		$display(A, "   -", B, "   =", out);	end
 
 
 
